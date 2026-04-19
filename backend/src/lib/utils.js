c68fafd8dc56c55fb53ab7810dc5a570750dbef0
Nitesh Kumavat <niteshkumavat32@gmail.com>
2026-04-19 23:44:23 +0530

    cookies

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,9 +10,10 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge : 7*24*60*60*1000,
         httpOnly : true,
-        sameSite : "strict",
+        sameSite : "none",
         secure : ENV.NODE_ENV === "development" ? false : true
     });
+
 
     return token;
 }
